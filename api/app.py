2b84499134cb4d722c4ba39af485b328ff73ddeb
Kushanava-Ghosh <134191735+Kushanava-Ghosh@users.noreply.github.com>
2025-01-11 18:26:30 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -69,7 +69,7 @@ def room_communication(room_id):
         if not message:
             return "Message cannot be empty."
 
-        rooms[room_id]["messages"] = (f"{client_ip}: {message}")
+        rooms[room_id]["messages"] = message
         return "Message sent successfully."
 
 if __name__ == "__main__":
